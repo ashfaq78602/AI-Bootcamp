f78b3548f012ebd11dc968252937bed1e5004de5
Ashfaq Ahmad Khan <ashfaq78602@gmail.com>
2025-09-18 12:14:38 +0200

    Lambda functions

--- Python Basics/python_project.py
+++ Python Basics/python_project.py
@@ -1,9 +1,13 @@
 #[expression for items in iterable if condition]
 
 #Create a list of squares
-squares = [x**2 for x in range(10)]
-print(squares)
+# squares = [x**2 for x in range(10)]
+# print(squares)
 
 #Filter even numbers
-evens = [x for x in range(10) if x % 2 == 0]
-print(evens)
+# evens = [x for x in range(10) if x % 2 == 0]
+# print(evens)
+
+#Lambda Functions - single expression functions
+add = lambda x, y: x + y
+print(add(3,5))
