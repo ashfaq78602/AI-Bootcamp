bd16ad5d5a9549e9f353bf18658886a1d31f1cbe
Ashfaq Ahmad Khan <ashfaq78602@gmail.com>
2025-09-18 14:26:07 +0200

    Added priority and deadline to task manager

--- Python Basics/python_project.py
+++ Python Basics/python_project.py
@@ -44,6 +44,7 @@
 
 #Project 1 : Command-Line Task Manager
 import os
+from datetime import date
 
 #File to store tasks
 FILE_NAME = "tasks.txt"
@@ -54,21 +55,21 @@ def load_tasks():
     if os.path.exists(FILE_NAME):
         with open(FILE_NAME, "r") as file:
             for line in file:
-                task_id, title, status = line.strip().split("|")
-                tasks[int(task_id)] = {"title": title, "status": status}
+                task_id, title, status, priority, deadline = line.strip().split("|")
+                tasks[int(task_id)] = {"title": title, "status": status, "priority": priority, "deadline": deadline}
     return tasks
 
 #Save tasks to file
 def save_tasks(tasks):
     with open(FILE_NAME, "w") as file:
         for task_id, task in tasks.items():
-            file.write(f"{task_id} | {task['title']} | {task['status']} \n")
+            file.write(f"{task_id} | {task['title']} | {task['status']}  | {task['priority']} | {task['deadline']}\n")
 
 #Add a new task
 def add_task(tasks):
     title = input("Enter task title:")
     task_id = max(tasks.keys(), default = 0) + 1
-    tasks[task_id] = {"title": title, "status": "Incomplete"}
+    tasks[task_id] = {"title": title, "status": "Incomplete", "priority": "Normal", "deadline": "None"}
     print(f"Task '{title}' added.")
     
 #View all tasks
@@ -77,7 +78,7 @@ def view_tasks(tasks):
         print("No tasks available.")
     else:
         for task_id, task in tasks.items():
-            print(f"[{task_id}] {task['title']} - {task['status']}")
+            print(f"[{task_id}] {task['title']} - {task['status']} - {task['priority']} - {task['deadline']}")
 
 #Mark task as complete
 def mark_task_complete(tasks):
@@ -85,6 +86,38 @@ def mark_task_complete(tasks):
     if task_id in tasks:
         tasks[task_id]["status"] = "complete"
         print(f"Task '{tasks[task_id]['title']}' marked as complete.")
+    else: 
+        print("Task ID not found.")
+        
+#Set Priority
+def set_priority(tasks):
+    task_id = int(input("Enter the task ID to set priority for: "))
+    if task_id in tasks:
+        priority_id = int(input("Enter which priority you want to set: (1. Normal 2. Urgent 3. Very Urgent): "))
+        if priority_id == 1:
+            tasks[task_id]['priority'] = "Normal"
+        elif priority_id == 2:
+            tasks[task_id]['priority'] = "Urgent"
+        elif priority_id == 3:
+            tasks[task_id]['priority'] = "Very Urgent"
+        else:
+            print("No Wrong Option Selected. Choose Again.")
+            set_priority(tasks)
+    else: 
+        print("Task ID not found.")
+        
+#Set Deadline
+def set_deadline(tasks):
+    task_id = int(input("Enter the task ID to set deadline for: "))
+    if task_id in tasks:
+        print(f"{tasks[task_id]} is selected.")
+        deadline = int(input("Enter deadline in days from current date: "))
+        try:
+            if deadline > 0:
+                tasks[task_id]['deadline'] = str(deadline) + " days from " + str(date.today())
+        except ValueError:
+            print("Input is not numeric. Enter again")
+            set_deadline(tasks)
     else: 
         print("Task ID not found.")
 
@@ -105,8 +138,10 @@ def main():
         print("1.Add Task")
         print("2. View Tasks")
         print("3. Mark Task as Complete")
-        print("4. Delete Task")
-        print("5. Exit")
+        print("4. Set Priority")
+        print("5. Set Deadline")
+        print("6. Delete Task")
+        print("7. Exit")
         choice = input("Enter your choice: ")
         
         if choice == "1":
@@ -116,8 +151,12 @@ def main():
         elif choice == "3":
             mark_task_complete(tasks)
         elif choice == "4":
-            delete_task(tasks)
+            set_priority(tasks)
         elif choice == "5":
+            set_deadline(tasks)
+        elif choice == "6":
+            delete_task(tasks)
+        elif choice == "7":
             save_tasks(tasks)
             print("Goodbye!!!")
             break
